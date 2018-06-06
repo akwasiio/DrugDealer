da824af94656f6cbad7c9c017ac7f174cfa57ccc
Kingsley <besi4lukas@gmail.com>
2018-06-06 14:51:20 +0000

    fourth commit
    added layouts and views

--- DrugApp_web/app/Http/Controllers/DrugsController.php
+++ DrugApp_web/app/Http/Controllers/DrugsController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class DrugsController extends Controller
 {
-    //
+    public function home(){
+        return view('home') ;
+    }
 }
--- DrugApp_web/routes/web.php
+++ DrugApp_web/routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['uses' => 'DrugsController@home']);
+
+
+#return view('welcome');
