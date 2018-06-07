d1aa2f20c59ae16060cb5b1c4fb6f67320ac4213
Kingsley <besi4lukas@gmail.com>
2018-06-07 08:42:41 +0000

    eight commit
    updated layout and home

--- DrugApp_web/resources/views/register.blade.php
+++ DrugApp_web/resources/views/register.blade.php
@@ -25,7 +25,7 @@
 
             <!--company name-->
             <div class="form-group row">
-                <label for="company_name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                <label for="company_name" class="col-md-4 col-form-label text-md-right">{{ __('company name') }}</label>
 
                 <div class="col-md-6">
                     <input type="text" class="form-control{{ $errors->has('company_name') ? ' is-invalid' : '' }}" name="company_name" value="{{ old('name') }}" required autofocus>
