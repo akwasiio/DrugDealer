0c0e1a1849cb8d4111a1f18eb33d7a562eedbb0a
namireku <nicholasmireku100@gmail.com>
2018-06-11 08:22:34 +0000

    User login interface

---
+++ DrugApp_android/app/src/main/java/code/shinobi/drugapp_android/UserAreaActiviity.kt
@@ -0,0 +1,12 @@
+package code.shinobi.drugapp_android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class UserAreaActiviity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_area_activiity)
+    }
+}
